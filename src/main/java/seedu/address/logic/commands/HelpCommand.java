420af14b84b0b23c9dd2125f9797c8cf939a6256
Zer0Legion <lengkhai@gmail.com>
2024-04-03 21:02:20 +0800

    Fix: remove pop up window for help command
    Now less mouse usage is required.
    
    Lets optimize for typists.

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -2,20 +2,30 @@ package seedu.address.logic.commands;
 
 import seedu.address.model.Model;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+
 /**
  * Format full help instructions for every command for display.
  */
 public class HelpCommand extends Command {
+    private static final StringSelection USER_GUIDE_URL =
+            new StringSelection("https://ay2324s2-cs2103t-t10-1.github.io/tp/UserGuide.html");
 
     public static final String COMMAND_WORD = "help";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
             + "Example: " + COMMAND_WORD;
+    public static final String SHOWING_HELP_MESSAGE =
+            "Copied user guide link to clipboard. Open the link in a browser to view it.";
 
-    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+    private void copyToClipboard(StringSelection stringSelection) {
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, stringSelection);
+    }
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        copyToClipboard(USER_GUIDE_URL);
+        return new CommandResult(SHOWING_HELP_MESSAGE, false, false);
     }
 }
