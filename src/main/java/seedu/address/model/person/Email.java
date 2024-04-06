c4418ea9779eaf6a5abcb34a1980c2cee2085367
delishad21 <soojohan@gmail.com>
2024-04-06 15:44:49 +0800

    Update error messages to match correct usage

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -18,7 +18,7 @@ public class Email {
             + "characters.\n"
             + "2. This is followed by a '@' and then a domain name. "
             + "The domain name is made up of domain labels "
-            + "separated by periods.\n"
+            + "which may be separated by periods.\n"
             + "The domain name must:\n"
             + "    - end with a domain label at least 2 characters long\n"
             + "    - have each domain label start and end with alphanumeric characters\n"
